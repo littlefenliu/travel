b896e8546f801c004ba7abb905882a2723919802
littlefenliu <silverliui@qq.com>
2020-11-04 20:15:32 +0800

    banner-finished

---
+++ src/common/gallary/Gallary.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="container" @click="handleGallaryClick">
+      <div class="wrapper">
+        <swiper :options="swiperOptions">
+            <swiper-slide 
+            v-for="(item,index) in imgs"
+            :key="index">
+            <img class='gallary-img' :src="item" alt="">
+            </swiper-slide>
+            <!-- 显示分页内容 小圆点-->
+            <div class="swiper-pagination" slot="pagination"></div>
+        </swiper>
+      </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+    name:'CommonGallary',
+     props:{
+         imgs:{
+            type: Array,
+            default (){
+                return []
+            }
+        }
+    },
+    data (){
+        return {
+            swiperOptions:{
+                pagination:'.swiper-pagination',
+                paginationType:'fraction',
+                observeParents:true,
+                observer:true
+            }
+        }
+    },
+    methods:{
+        handleGallaryClick(){
+            this.$emit('close')
+       }
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+.container>>>.swiper-container
+    overflow inherit
+.container
+    display flex
+    flex-direction column
+    justify-content center
+    position fixed
+    z-index 99
+    left 0
+    top 0
+    right 0
+    bottom 0
+    background-color #000
+    .wrapper
+        height 0
+        width 100%
+        padding-bottom 63%
+        background-color #fff
+        .gallary-img
+            width 100%
+        .swiper-pagination
+            bottom -1rem
+            color #fff
+</style>
---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,79 @@
+<template>
+<div>
+    <div class="banner" @click="handleBannerClick">
+        <img class="banner-img" 
+            src="//img1.qunarzz.com/sight/p0/1508/f7/f7c3be996152011c.img.jpg_600x330_dfb694b6.jpg" alt="">
+        <div class="banner-info">
+            <div class="banner-title">南京总统府(AAAA景区)</div>
+            <div class="banner-number">
+                <span class="iconfont banner-icon">&#xe666;</span>
+                11
+            </div>
+        </div>
+    </div>
+    <common-gallary 
+    :imgs="imgs" 
+    v-show="showGallary"
+    @close="handleGallaryClose"
+    ></common-gallary>
+</div>
+</template>
+
+<script>
+import CommonGallary from 'common/gallary/Gallary'
+export default {
+    name:'DetailBanner',
+    components:{
+        CommonGallary
+    },
+    data (){
+        return {
+            showGallary:false,
+            imgs:['http://img1.qunarzz.com/sight/p0/1508/fc/fcc02b326b402ea6.img.jpg_r_800x800_cfdb87e5.jpg',
+                'http://img1.qunarzz.com/sight/p0/1508/ab/ab2dfd426a6a4bcf.img.jpg_r_800x800_729b24b2.jpg']
+            }
+    },
+    methods:{
+        handleBannerClick(){
+            this.showGallary=true
+        },
+        handleGallaryClose(){
+            this.showGallary=false
+        }
+    }
+        
+}
+</script>
+
+<style lang="stylus" scoped>
+.banner
+    position relative
+    overflow hidden
+    height 0
+    padding-bottom: 55%
+    .banner-img
+        width 100%
+    .banner-info
+        display flex
+        position absolute
+        left 0
+        right 0
+        bottom 0
+        line-height .6rem
+        color #fff
+        background-image linear-gradient(top,rgba(0,0,0,0),rgba(0,0,0,0.8)) 
+        .banner-title
+            font-size .3rem
+            padding 0 .2rem
+            flex 1
+        .banner-number
+            margin-top .2rem
+            padding 0 .4rem
+            line-height .4rem
+            height .32rem
+            border-radius .2rem
+            background rgba(0,0,0,.8)
+            font-size .30rem
+        .banner-icon
+            font-size .30rem
+</style>
